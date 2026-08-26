0ea45b353bf45b5926080ffb8a139cf6c9a36aa0
Anushka Sharma <144245430+Anushka4404@users.noreply.github.com>
2026-08-26 17:03:45 +0530

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.java
@@ -0,0 +1,37 @@
+// class Solution {
+//     public int canCompleteCircuit(int[] gas, int[] cost) {
+//         int n = gas.length;
+//         int start = n-1, end = 0;
+//         int tank = gas[start] - cost[start];
+//         while (start>end) {
+//             if (tank<0) {
+//                 start--;
+//                 tank+=gas[start]-cost[start];
+//             }
+//             else {
+//                 tank+=gas[end]-cost[end];
+//                 end++;
+//             }
+//         }
+//         return tank>=0?start:-1;
+//     }
+// }
+
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        if (Arrays.stream(gas).sum() < Arrays.stream(cost).sum()) {
+            return -1;
+        }
+        int total = 0,res=0;
+        for (int i=0;i<gas.length;i++) {
+            total+=(gas[i]-cost[i]);
+            if (total<0) {
+                total =0;
+                res=i+1;
+            }
+        }
+        return res;
+    }
+}
